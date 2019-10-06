cdd32d8cf0cec55e4383722b55904df052b0e7ae
njoroge33 <johngichuhi769@gmail.com>
2019-10-06 20:40:38 +0300

    add a functionality to add the total to be paid by the user

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+function getSize(type, size) {
+    var chicken = [600, 800, 1200];
+    var meaty = [550, 700, 1000];
+    var vegetarian = [500, 600, 900];
+
+    if (type === "chicken") {
+        return chicken[size];
+    } else if (type === "meaty") {
+        return meaty[size];
+    } else if (type === "vegetarian") {
+        return vegetarian[size];
+    }
+}
+
 $(document).ready(function () {
     $("#menu").click(function () {
         $(".about").hide();
@@ -8,4 +22,40 @@ $(document).ready(function () {
         $(".menu").hide();
         $(".about").toggle();
     });
+
+    $('#submit').click(function (event) {
+        event.preventDefault();
+        var type = $("input[name='type']:checked").val();
+        var size = parseInt($("input[name='size']:checked").val());
+        var crust = parseInt($("input[name='crust']:checked").val());
+        var toppings = parseInt($("input[name='topping']:checked").val());
+        var amount = parseInt($("#number").val());
+        var total = (getSize(type, size) + crust + toppings) * amount;
+
+        if (amount <= 0) {
+            amount = 1
+        }
+
+        if (Number.isNaN(total) === true) {
+            alert("CONFIRM ATLEAST YOU HAVE SELECTED ONE ITEM IN EACH SECTION!");
+        } else {
+            $("#post").html("Total" + " : Kshs" + " " + total);
+            $("#out").show();
+            $(".post").css('background-color', 'rgb(27, 1, 1)')
+        }
+    })
+
+    $("#out").click(function () {
+        event.preventDefault();
+        var deliver = confirm("Would you like us to deliver your order?");
+
+        if (deliver) {
+            var place = prompt("Enter your location:");
+            $("#post").html(`Your order will be delivered at  ${place} in 30 mins`);
+
+        } else {
+            alert("Collect your order at our outlet near you");
+        }
+        setTimeout(function () { location.reload(); }, 5000);
+    })
 });
